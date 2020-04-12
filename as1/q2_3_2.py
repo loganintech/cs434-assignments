45940ff2c50bdfa456b1d4d89cb7e507de9aee1a
Logan Saso <logansaso@gmail.com>
2020-04-11 22:03:19 -0700

    All the things

--- as1/q2_3_2.py
+++ as1/q2_3_2.py
@@ -7,41 +7,45 @@ import matplotlib.pyplot as plt
 
 
 def load_files(training, testing):
-    trdata = np.genfromtxt(training, delimiter=",")
-    tedata = np.genfromtxt(testing, delimiter=",")
-    tr_feat = trdata[:, :-1] / 255.0
-    tr_exp = trdata[:, -1]
-    te_feat = tedata[:, :-1] / 255.0
-    te_exp = tedata[:, -1]
+    tr_feat = np.genfromtxt(training, usecols=range(256), delimiter=",")
+    tr_feat /= 255.0
+    tr_feat = np.insert(tr_feat, 0, 1, axis=1)
+    tr_exp = np.genfromtxt(training, usecols=range(-1), delimiter=",")
+
+    te_feat = np.genfromtxt(testing, usecols=range(256), delimiter=",")
+    te_feat /= 255.0
+    te_feat = np.insert(te_feat, 0, 1, axis=1)
+    te_exp = np.genfromtxt(testing, usecols=range(-1), delimiter=",")
+
     return tr_feat, tr_exp, te_feat, te_exp
 
 
 def sigmoid(weight, case):
-    exponent = np.dot(-weight, case)
+    # try:
+    exponent = -1.0 * np.dot(weight.T, case)
 
     try:
-        prediction = 1.0 / (1.0 + math.exp(exponent.item(0)))
+        prediction = 1.0 / (1.0 + np.exp(exponent))
     except Exception as e:
-        # print(f"Exponent: {exponent}")
-        # print(f"Exponent Item: {exponent.item(0)}")
+        print(f"Exponent: {exponent}")
+        print(f"Exponent Item: {exponent.item(0)}")
         prediction = 1.0 / (1.0 + mp.exp(exponent.item(0)))
     return prediction
 
 
 def check_accuracy(w, x, y):
     correct = 0
-
     for (case, expected) in zip(x, y):
-        if expected == 1 and sigmoid(w, case) >= 0.5:
+        if expected[0] == 1.0 and np.dot(w.T, case) >= 0.0:
             correct += 1
-        elif expected == 0 and 1 - sigmoid(w, case) >= 0.5:
+        elif expected[0] == 0.0 and 1 - np.dot(w.T, case) < 0.0:
             correct += 1
 
     percentage_correct = correct / x.shape[0]
     return percentage_correct
 
 
-def gradient(training_data, training_expected, testing_data, testing_expected, reg_strength=None, iterations=100):
+def gradient(training_data, training_expected, testing_data, testing_expected, reg_strength=None, iterations=250):
     training_accuracies = []
     testing_accuracies = []
 
@@ -51,24 +55,24 @@ def gradient(training_data, training_expected, testing_data, testing_expected, r
         except:
             reg_strength = None
 
-    w = np.zeros((1, 256))
-    regularization_val = 0
+    w = np.zeros(training_data.shape[1])  # Feature count
 
     learning_rate = 0.000005  # let the learning rate be very small
 
     for _ in range(iterations):
-        batch = np.zeros((1, 256))
-        for i, case in enumerate(training_data):
-            predicted = sigmoid(w, case)
-            diff = (predicted - training_expected[i]) * case
-            batch = np.add(batch, diff)
+        gradient_batch = np.zeros(training_data.shape[1])  # Feature count
+        for i in range(training_data.shape[0]): # Example count
+            predicted = sigmoid(w, training_data[i])
+            diff = (predicted - training_expected[i]) * training_data[i]
+            gradient_batch = np.add(gradient_batch, diff)
 
         if reg_strength is not None:
             normalized = np.linalg.norm(w) ** 2
-            batch = np.add(batch, normalized * 0.5 * reg_strength)
+            gradient_batch = np.add(
+                gradient_batch, normalized * 0.5 * reg_strength)
 
-        batch = learning_rate * batch
-        w = np.subtract(w, batch)
+        gradient_batch = learning_rate * gradient_batch
+        w = np.subtract(w, gradient_batch)
 
     train_acc = check_accuracy(w, training_data, training_expected)
     test_acc = check_accuracy(w, testing_data, testing_expected)
@@ -95,6 +99,8 @@ if __name__ == "__main__":
             training_features, training_expected, test_features, test_expected, regularization_strength)
         tr_acc.append(training_acc)
         te_acc.append(testing_acc)
+
+    print(tr_acc, te_acc)
 
     plt.ylabel("Accuracy")
     plt.xlabel("Lambda")
