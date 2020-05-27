07fcca2f2c26bec44bf3390f1c87765c69e31431
kulkarnr <43656882+kulkarnr@users.noreply.github.com>
2020-05-27 01:57:39 -0500

    Update utils.py

--- as4/src/utils.py
+++ as4/src/utils.py
@@ -6,15 +6,17 @@ import seaborn as sns
 sns.set()
 
 
-def load_data(rootdir='./'):
+def load_data(rootdir='./data/'):
     print('load data \n')
-    x_train = np.loadtxt(rootdir + 'x_train.txt', dtype=str).astype(float)
-    y_train = np.loadtxt(rootdir + 'y_train.txt', dtype=str).astype(int)
-    x_test = np.loadtxt(rootdir + 'x_test.txt', dtype=str).astype(float)
-    y_test = np.loadtxt(rootdir + 'y_test.txt', dtype=str).astype(int)
+    c = './data/x_train.txt'
+    c2 = './data/y_train.txt'
+    #x_train = np.loadtxt(rootdir + 'x_train.txt', dtype=str).astype(float)
+    x_train = np.loadtxt(c, dtype=str).astype(float)
+    #y_train = np.loadtxt(rootdir + 'y_train.txt', dtype=str).astype(int)
+    y_train = np.loadtxt(c2, dtype=str).astype(int)
+    #x_test = np.loadtxt(rootdir + 'x_test.txt', dtype=str).astype(float)
+    #y_test = np.loadtxt(rootdir + 'y_test.txt', dtype=str).astype(int)
 
-    print('x_train: [%d, %d], y_train:[%d,], x_test: [%d, %d], y_test:[%d]' % (
-        x_train.shape[0], x_train.shape[1], y_train.shape[0], x_test.shape[0],
-        x_test.shape[1], y_test.shape[0]))
+    print('x_train: [%d, %d], y_train:[%d,]' % (x_train.shape[0], x_train.shape[1], y_train.shape[0]))
 
-    return x_train, y_train, x_test, y_test
+    return x_train, y_train#, x_test, y_test
